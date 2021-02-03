0d37dd49f66f24a758f9b8270483fb60b265b00f
zrz <316677100@qq.com>
2021-02-04 00:00:20 +0800

    finish cross origin problem

--- src/main/java/com/zrz/personalWeb/controller/UserController.java
+++ src/main/java/com/zrz/personalWeb/controller/UserController.java
@@ -3,66 +3,87 @@ package com.zrz.personalWeb.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
 import com.zrz.personalWeb.domain.User;
 import com.zrz.personalWeb.service.UserService;
 
-import javax.servlet.http.HttpSession;
-
 @RestController
+@CrossOrigin(maxAge = 3600)
 public class UserController {
 
 	@Autowired
 	UserService uservice;
 
 	@RequestMapping(value = "/loginIn", method = RequestMethod.POST)
-	public String loginIn(@RequestParam("username") String name, @RequestParam("pwd") String pwd, HttpSession sess) {
+	@ResponseBody
+	public Object loginIn(@RequestBody JSONObject jsobj, HttpServletRequest request) throws JSONException {
+		String name = jsobj.get("username").toString();
+		String pwd = jsobj.get("pwd").toString();
+
+		HttpSession sess = request.getSession();
+
 		User us = uservice.login(name, pwd);
 		if (us != null) {
 			sess.setAttribute("uid", us.getId());
 			sess.setAttribute("uname", us.getName());
-			return "success\n" + us.getId() + "\n" + us.getName() + "\n";
+			return JSON.toJSONString("success");
 		}
-		else
-			return "error\n";
+		else {
+			return JSON.toJSONString("error");
+		}
 	}
 
 	@RequestMapping(value = "/registerIn", method = RequestMethod.POST)
-	public String registerIn(@RequestParam("username") String name, @RequestParam("pwd") String pwd,
-			@RequestParam("email") String email) {
+	@ResponseBody
+	public Object registerIn(@RequestBody JSONObject jsobj) {
+		String name = jsobj.get("username").toString();
+		String pwd = jsobj.get("pwd").toString();
+		String email = jsobj.get("email").toString();
+		
 		if (uservice.register(name, pwd, email))
-			return "success\n";
-		return "error\n";
+			return JSON.toJSONString("success");
+		
+		return JSON.toJSONString("error");
 	}
 
-	@RequestMapping(value = "/info", method = RequestMethod.POST)
+	@RequestMapping(value = "/info", method = {RequestMethod.POST, RequestMethod.GET})
 	@ResponseBody
-	public Map<String, Object> showUserInfo(@RequestParam("username") String name) {
+	public String showUserInfo(HttpServletRequest request) {
 		Map<String, Object> mp = new HashMap<>();
-		User user = uservice.showInfo(name);
+		
+		User user = uservice.showInfo(request.getSession().getAttribute("uname").toString());
+		
 		mp.put("id", user.getId());
 		mp.put("name", user.getName());
 		mp.put("mail", user.getEmail());
-		return mp;
+		return JSON.toJSONString(mp);
 	}
 
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	public String updateUserInfo(String name, String newName, String newMail) {
 		if(uservice.updateInfo(name, newName, newMail) == true)
-			return "success\n";
-		return "error\n";
+			return "success";
+		return "error";
 	}
 
 	@RequestMapping(value = "/updatepwd", method = RequestMethod.POST)
 	public String updatePwd(String name, String newPwd) {
 		if(uservice.updatePwd(name, newPwd) == true)
-			return "success\n";
-		return "error\n";
+			return "success";
+		return "error";
 	}
 }
